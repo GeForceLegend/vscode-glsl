6ef8538ddb6ac700922cbbe55c9271bbe40e0778
GeForceLegend <512287858@qq.com>
2022-12-12 10:20:25 +0800

    Deleted unused functions and files

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,9 @@
 'use strict';
 
 import * as vscode from 'vscode';
-// import * as semantic from './semantic';
 import * as completion from './completion';
 
-
 export function activate(context: vscode.ExtensionContext) {
-	// context.subscriptions.push(semantic.semanticTokensProvider);
 	completion.completionItemProviders.forEach((completionItemProvider) => {
 		context.subscriptions.push(completionItemProvider);
 	});
